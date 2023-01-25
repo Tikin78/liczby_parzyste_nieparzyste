34d8ef882fc246f759b217e819968db6f355d601
nauczyciel <tikin78@gmail.com>
2023-01-25 16:53:30 +0100

    Add projekt plikow

---
+++ LiczbaParzystaNieparzysta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiczbaParzystaNieparzysta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą a ja powiem Ci czy liczba jest parzysta czy nieparzysta");
+
+            var liczba = GetLiczba();
+            
+            //var liczba = int.Parse(Console.ReadLine());
+            
+            while(true) 
+            {
+                if((liczba % 2)==0) 
+                {
+                    Console.WriteLine("Liczba parzysta");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("Liczba nieparzysta");
+                    break;
+                
+                }
+            
+            }
+
+        }
+
+        private static int GetLiczba()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int liczba))
+                    
+                {
+                    Console.WriteLine("Podałeś nie prawidłowe dane. Spróbój ponownie.");
+                    continue;
+                }
+                return liczba;
+            }
+        }
+    }
+}
